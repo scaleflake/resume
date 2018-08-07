a273753353a4287c03b1d5d16a5cea2ae017c5a8
scaleflake <scaleflake@gmail.com>
2018-08-07 06:56:24 +0300

    0.02

--- server.js
+++ server.js
@@ -3,26 +3,20 @@
 var express = require('express');
 var app = express();
 
-// app.use('/', express.static(__dirname + '/'));
+app.use('/', express.static(__dirname + '/'));
 
-function sendEn(req, res) {
-	console.log('EN');
-	res.sendFile(__dirname + '/index.html');
-}
+
 
 ['/resume', '/resume-en', '/resume_en'].forEach(e => {
-	app.use(e, express.static(__dirname + '/'));
-	app.get(e, sendEn);
+	app.get(e, function(req, res) {
+		res.sendFile(__dirname + '/index.html');
+	});
 });
 
-function sendRu(req, res) { 
-	console.log('RU');
-        res.sendFile(__dirname + '/index-ru.html');
-}
-
 ['/resume-ru', '/resume_ru'].forEach(e => {
-	app.use(e, express.static(__dirname + '/'));
-	app.get(e, sendRu);
+	app.get(e, function(req, res) { 
+	    res.sendFile(__dirname + '/index-ru.html');
+	});
 });
 
 app.listen(18000);
