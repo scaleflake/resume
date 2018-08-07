b08d462abf7e89bef6671bdb93de44fdb176098f
scaleflake <scaleflake@gmail.com>
2018-08-07 04:54:34 +0300

    -

---
+++ server.js
@@ -0,0 +1,21 @@
+/* jshint esversion: 6*/
+
+var express = require('express');
+var app = express();
+
+app.use('/', express.static(__dirname + '/'));
+
+['/resume', '/resume/en', '/resume-en', '/resume_en'].forEach(e => {
+	app.get(e, (req, res) => {
+		res.sendFile(__dirname + '/index.html');
+	});
+});
+
+['/resume/ru', '/resume-ru', '/resume_ru'].forEach(e => {
+	app.get(e, (req, res) => {
+		res.sendFile(__dirname + '/index-ru.html');
+	});
+});
+
+app.listen(8080);
+console.log('Server started');
